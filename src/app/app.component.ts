59d0065c57db870e76ab097bca825e8d5fe51d3d
AlexandreAlmaraz <alexalmarazm14@gmail.com>
2024-12-12 19:15:54 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DirectivasComponent } from './directivas/directivas.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet, EncabezadoComponent, DirectivasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'aam-ejercicio14';
 }
